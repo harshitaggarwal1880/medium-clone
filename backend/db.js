e74f6415512c0574951a925efa897d1c91ce2f65
harshit <harshit@zeddlabz.com>
2023-06-05 23:17:33 +0530

    Done

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb+srv://root:root@cluster0.0e9rx7f.mongodb.net/?retryWrites=true&w=majority";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
